25fd6ca7ddbd861156ec646a76ddf0655b51c9c1
Jorge Ossa <jorgehossa@gmail.com>
2018-10-12 15:54:32 -0500

    js innerHTML the result

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const $button = document.querySelector('#button');
 const $income = document.querySelector('#income');
 const $output = document.querySelector('#output');
-const porcentaje = 0.10;
+const tithePercent = 0.10;
+//input mask
 
 
+//Button take input value
 $button.addEventListener('click', () => {
-  console.log($income.value * porcentaje);
+  //let roundNumber = $income.value.replace(',', '');
+  $output.innerHTML = ($income.value * tithePercent);
+  //console.log(roundNumber * tithe)
 })
