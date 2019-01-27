5c32ae7a9773011f8f8c2e10f7755c62c3f7fb9f
Jorge Ossa <jorgehossa@gmail.com>
2019-01-26 20:43:31 -0500

    Improvement in javascript file

--- js/main.js
+++ js/main.js
@@ -9,16 +9,21 @@ var cleave = new Cleave('.income', {
   numeralThousandsGroupStyle: 'thousand'
 });
 
-//Button take input value
-$button.addEventListener('click', () => {
+//Calc Function
+function titheCalc () {
   let value = $income.value.replace(/,/g, '')
   let roundNumber = Number(value)
   //$output.innerHTML = (roundNumber * tithePercent);
   let titheTotal = roundNumber * tithePercent
   $output.innerHTML = (titheTotal.toLocaleString('es-CO', {minimumFractionDigits: 0, maximumFractionDigits: 0 }));
-})
-//reset button
-$resetButton.addEventListener('click', () => {
+}
+// Reset Function
+function resetInput () {
   $income.value = '';
   $output.innerHTML = ('')
-})
+}
+
+//Button take input value
+$button.addEventListener('click', titheCalc)
+//reset button
+$resetButton.addEventListener('click', resetInput)
