def342026789d807cf30696e5aa7a94cd960f3b5
Jorge Ossa <jorgehossa@gmail.com>
2019-01-26 22:50:21 -0500

    Start whit api implementation

--- js/main.js
+++ js/main.js
@@ -16,11 +16,16 @@ function titheCalc () {
   //$output.innerHTML = (roundNumber * tithePercent);
   let titheTotal = roundNumber * tithePercent
   $output.innerHTML = (titheTotal.toLocaleString('es-CO', {minimumFractionDigits: 0, maximumFractionDigits: 0 }));
-}
+};
 // Reset Function
 function resetInput () {
   $income.value = '';
   $output.innerHTML = ('')
+};
+// Fetch Bible API
+async function API(url){
+  const endpoint = 'https://gist.githubusercontent.com/jorgehossa/1fb82ea5b25bfeb1af979cd4ea8bc00b/raw/9eaeb436b6a0107ef35a1b770647369a5a33d87b/es_rves_rvr.json';
+  const response = await fetch(endpoint)
 }
 
 //Button take input value
