cd07ff2f11ef5557c79a782c12c8ca9a1ce25a33
Jorge Ossa <jorgehossa@gmail.com>
2019-01-31 01:18:46 -0500

    Set some styles and activate copyClipboard function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const $button = document.querySelector("#button");
 const $resetButton = document.querySelector("#resetButton");
+const $copyButton = document.querySelector("#copyButton");
 const $income = document.querySelector("#income");
 const $output = document.querySelector("#output");
 const $container = document.querySelector("main");
@@ -17,32 +18,34 @@ function titheCalc() {
   let value = $income.value.replace(/,/g, "");
   let roundNumber = Number(value);
   let titheTotal = roundNumber * tithePercent;
-  const markdown = `
-    <span>${verseText}</span><br><br>
-    <a href="${verseLink}">${verseRef}</a>
-  `;
-  $output.innerHTML = titheTotal.toLocaleString("es-CO", {
+  let total = titheTotal.toLocaleString("es-CO", {
     minimumFractionDigits: 0,
     maximumFractionDigits: 0
   });
-  $verse.innerHTML = markdown;
+  const markdown = `
+    <span>${verseText}</span><br><br>
+    <a href="${verseLink}">${verseRef}</a>
+  `
+  $output.innerHTML = total
+  $verse.innerHTML = markdown
+  copyToClipboard(total)
 }
-
-function copyToClipboard(text){
-  var dummy = document.createElement("input");
-  document.body.appendChild(dummy);
-  dummy.setAttribute('value', text);
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
-}
-
 
 // Reset Function
 function resetInput() {
   $income.value = "";
   $output.innerHTML = "";
   $verse.innerHTML = "";
+}
+
+// Copy function
+function copyToClipboard(text) {
+  var dummy = document.createElement("input");
+  document.body.appendChild(dummy);
+  dummy.setAttribute("value", text);
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
 }
 
 // Bible API
