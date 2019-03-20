202a27f9ba92c9536bb03dd0fcf52e87d3754355
Jorge Ossa <jorgehossa@gmail.com>
2019-03-20 17:05:29 -0500

    Prettier Fix

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
-const $button = document.querySelector("#button")
-const $resetButton = document.querySelector("#resetButton")
-const $copyButton = document.querySelector("#copyButton")
-const $income = document.querySelector("#income")
-const $output = document.querySelector("#output")
-const $container = document.querySelector("main")
-const $verse = document.querySelector(".verse")
+const $button = document.querySelector('#button');
+const $resetButton = document.querySelector('#resetButton');
+const $copyButton = document.querySelector('#copyButton');
+const $income = document.querySelector('#income');
+const $output = document.querySelector('#output');
+const $container = document.querySelector('main');
+const $verse = document.querySelector('.verse');
 
-//input mask by Cleave
-var cleave = new Cleave(".income", {
+// input mask by Cleave
+const cleave = new Cleave('.income', {
   numeral: true,
-  numeralThousandsGroupStyle: "thousand"
-})
+  numeralThousandsGroupStyle: 'thousand',
+});
 
-//Calc Function
+// Copy function
+function copyToClipboard(text) {
+  const dummy = document.createElement('input');
+  document.body.appendChild(dummy);
+  dummy.setAttribute('value', text);
+  dummy.select();
+  document.execCommand('copy');
+  document.body.removeChild(dummy);
+}
+
+// Calc Function
 function titheCalc() {
   const tithePercent = 0.1;
-  let value = $income.value.replace(/,/g, "");
-  let roundNumber = Number(value);
-  let titheTotal = roundNumber * tithePercent;
-  let total = titheTotal.toLocaleString("es-CO", {
+  const value = $income.value.replace(/,/g, '');
+  const roundNumber = Number(value);
+  const titheTotal = roundNumber * tithePercent;
+  const total = titheTotal.toLocaleString('es-CO', {
     minimumFractionDigits: 0,
-    maximumFractionDigits: 0
+    maximumFractionDigits: 0,
   });
   const markdown = `
     <span>${verseText}</span><br><br>
     <a href="${verseLink}">${verseRef}</a>
-  `
-  $output.innerHTML = total
-  $verse.innerHTML = markdown
-  copyToClipboard(total)
+  `;
+  $output.innerHTML = total;
+  $verse.innerHTML = markdown;
+  copyToClipboard(total);
 }
 
 // Reset Function
 function resetInput() {
-  $income.value = "";
-  $output.innerHTML = "";
-  $verse.innerHTML = "";
-}
-
-// Copy function
-function copyToClipboard(text) {
-  var dummy = document.createElement("input");
-  document.body.appendChild(dummy);
-  dummy.setAttribute("value", text);
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
+  $income.value = '';
+  $output.innerHTML = '';
+  $verse.innerHTML = '';
 }
 
 // Bible API
@@ -56,10 +56,10 @@ fetch(
   `https://developers.youversionapi.com/1.0/verse_of_the_day/${day}?version_id=1`,
   {
     headers: {
-      "X-YouVersion-Developer-Token": "ChY3zDzH34aBkd42GfDl7i4rdXo",
-      "Accept-Language": "en",
-      Accept: "application/json"
-    }
+      'X-YouVersion-Developer-Token': 'ChY3zDzH34aBkd42GfDl7i4rdXo',
+      'Accept-Language': 'en',
+      Accept: 'application/json',
+    },
   }
 )
   .then(response => response.json())
@@ -70,8 +70,8 @@ fetch(
     verseLink = data.verse.url;
 
     // Calling titheCalc Function
-    $button.addEventListener("click", titheCalc);
+    $button.addEventListener('click', titheCalc);
   });
 
-//reset button
-$resetButton.addEventListener("click", resetInput);
+// reset button
+$resetButton.addEventListener('click', resetInput);
